e8dd743e2b122348ded7ca4566322c317e18e5a8
lamlopez-2020 <monagasweb2020@gmail.com>
2022-11-04 22:04:38 -0400

    refactoring-index.js and server.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.clear();
-import dotenv from "dotenv";
 import express from "express";
 
 import cookieParser from "cookie-parser";
@@ -8,15 +6,7 @@ import authRouter from "./routes/auth.js";
 import authSessionRouter from "./routes/auth_session.js";
 import authTokenRouter from "./routes/auth_token.js";
 
-dotenv.config();
-
-const PORT = Number(process.env.PORT) || 3000;
-
 const app = express();
-
-//Setting
-app.set("appName", "Node Course Desarrollo Util");
-app.set("port", PORT);
 
 //Middleware: función que se ejecuta para uno o  múltiples endpoints
 app.use(cookieParser());
@@ -30,9 +20,5 @@ app.use("/auth", authRouter);
 app.use("/auth-session", authSessionRouter);
 
 app.use("/auth-token", authTokenRouter);
-
-/* app.listen(PORT, () =>
-  console.log(`Server ${app.get("appName")} on port ${app.get("port")}`)
-); */
 
 export default app;
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 //import express from "express";
+console.clear();
+import dotenv from "dotenv";
 import app from "./index.js";
 
+dotenv.config();
+
+const PORT = Number(process.env.PORT) || 3000;
+
+//Setting
+app.set("appName", "Node Course Desarrollo Util");
+app.set("port", PORT);
 //const PORT = Number(process.env.PORT) || 3000;
 
 const server = app.listen(app.get("port"), () =>
